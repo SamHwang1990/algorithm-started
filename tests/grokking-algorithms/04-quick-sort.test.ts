579cf5ebbe6524d271f18f9dcfca784c72e21b49
SamHwang <sam@ProSam.local>
2018-07-11 00:06:53 +0800

    test: 补充快速排序测试用例

--- tests/grokking-algorithms/04-quick-sort.test.ts
+++ tests/grokking-algorithms/04-quick-sort.test.ts
@@ -13,5 +13,12 @@ describe('Asc Quick Sort test case', () => {
         let sortArr = quickAscSort(oriArr);
 
         assert(sortArr.join(',') === '-1000,3,33,49,84,90,100.1');
+    });
+
+    it('repeat element sort', () => {
+        let oriArr = [5, 9, 9, 5, 3, 4, 3, 4];
+        let sortArr = quickAscSort(oriArr);
+
+        assert(sortArr.join(',') === '3,3,4,4,5,5,9,9');
     })
 });
